55a31c63f767fc87da2efdfd1ad8e1286c93eca1
Mehedi <mehedi.interface@gmail.com>
2026-06-05 11:10:05 +0600

    example-1.js

---
+++ example-1.js
@@ -0,0 +1,19 @@
+const product = {
+    name: "Tea Leaf", 
+    price: 490, 
+    stock: 100
+}; 
+
+console.log(product.name); 
+console.log(product.price); 
+
+/*
+Key Concepts: 
+* Objects store data as key-value pairs.
+* Objects are created using {}.
+* Properties can be accessed using:
+    - object.key
+    - object["key"]
+* Properties can be added, updated, or deleted.
+* Objects can contain functions (methods).
+*/
